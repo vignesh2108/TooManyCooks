ee088f707586fcd39655b65aa2d0b73d46793b66
Vignesh Karthikeyan <vignesh2108@gmail.com>
2020-12-08 15:22:17 -0700

    dialog system implemented

---
+++ TooManyCooksUnity/Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialog
+{
+    // Start is called before the first frame update
+    public string name;
+    public string[] sentences;
+}
---
+++ TooManyCooksUnity/Assets/Scripts/Dialog/DialogManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text nameText;
+    public Text dialogText;
+    public Animator animator;
+    private Queue<string> sentences;
+    void Start()
+    {
+        sentences= new Queue<string>();
+    }
+
+    public void StartDialog(Dialog dialog)
+    {    
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialog.name;
+        sentences.Clear();
+        foreach (string sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogText.text = sentence;
+    }
+
+    void EndDialog()
+    {    
+        animator.SetBool("IsOpen", false);
+        Debug.Log("End of convo");
+    }
+
+    // Update is called once per frame
+    
+}
