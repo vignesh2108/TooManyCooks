72ce45d61ffaeb14b2b480ad7c471e6968bbf47f
Vignesh Karthikeyan <vignesh2108@gmail.com>
2020-12-08 15:52:31 -0700

    Dialog updated kepress p

--- TooManyCooksUnity/Assets/Scripts/GameManager.cs
+++ TooManyCooksUnity/Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     public static GameManager S;
     public GameObject sceneObjectPrefab;
     public bool isMobile;
+    public Dialog dialog;
     private void Awake()
     {
         if (S == null)
@@ -29,6 +30,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            FindObjectOfType<DialogManager>().StartDialog(dialog);
+        }
     }
 }
